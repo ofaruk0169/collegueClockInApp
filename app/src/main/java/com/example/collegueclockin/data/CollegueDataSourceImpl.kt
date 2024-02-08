0bd3811a392dad807637c84febd4de637e8c1847
Omare Faruk <ofaruk0169@gmail.com>
2024-02-08 21:15:22 +0000

    added new methods

--- app/src/main/java/com/example/collegueclockin/data/CollegueDataSourceImpl.kt
+++ app/src/main/java/com/example/collegueclockin/data/CollegueDataSourceImpl.kt
@@ -1,10 +1,41 @@
 package com.example.collegueclockin.data
 
+import app.cash.sqldelight.coroutines.asFlow
+import app.cash.sqldelight.coroutines.mapToList
 import com.example.collegueclockin.CollegueClockInDatabase
+import databases.CollegueEntity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.withContext
 
 
 class CollegueDataSourceImpl (
     db: CollegueClockInDatabase
-) {
+): CollegueDataSource {
 
+    private val queries = db.collegueEntityQueries
+
+    override suspend fun getPersonById(id: Long): CollegueEntity? {
+        return withContext(Dispatchers.IO) {
+            queries.getPersonById(id).executeAsOneOrNull()
+        }
+    }
+
+
+    override fun getAllPersons(): Flow<List<CollegueEntity>> {
+        return queries.getAllPersons().asFlow().mapToList(Dispatchers.IO)
+    }
+
+
+    override suspend fun deletePersonById(id: Long) {
+        withContext(Dispatchers.IO) {
+            queries.deletePersonById(id)
+        }
+    }
+
+    override suspend fun insertPerson(firstName: String, lastName: String, id: Long?) {
+        withContext(Dispatchers.IO) {
+            queries.insertPerson(id, firstName, lastName)
+        }
+    }
 }
---
+++ app/src/main/java/com/example/collegueclockin/viewmodels/PersonListViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.collegueclockin.viewmodels
+
+class PersonListViewModel {
+}

3ebfd59480fd59a18b9b8434d4e2a2566a12944c
Omare Faruk <ofaruk0169@gmail.com>
2024-02-13 11:39:33 +0000

    added viewmodel for db

--- app/src/main/java/com/example/collegueclockin/viewmodels/PersonListViewModel.kt
+++ app/src/main/java/com/example/collegueclockin/viewmodels/PersonListViewModel.kt
@@ -1,5 +1,62 @@
 package com.example.collegueclockin.viewmodels
 
-class PersonListViewModel {
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.collegueclockin.data.CollegueDataSource
+import databases.CollegueEntity
+import kotlinx.coroutines.launch
 
+class PersonListViewModel(
+    private val collegueDataSource: CollegueDataSource
+): ViewModel() {
+
+
+    val collegues = collegueDataSource.getAllPersons()
+
+    var personDetails by mutableStateOf<CollegueEntity?>(null)
+        private set
+
+    var firstNameText by mutableStateOf("")
+        private set
+
+    var lastNameText by mutableStateOf("")
+        private set
+
+    fun onInsertPersonClick() {
+        if(firstNameText.isBlank() || lastNameText.isBlank()) {
+            return
+        }
+        viewModelScope.launch {
+            collegueDataSource.insertPerson(firstNameText, lastNameText)
+            firstNameText = ""
+            lastNameText = ""
+        }
+    }
+
+    fun onDeleteClick(id: Long) {
+        viewModelScope.launch {
+            collegueDataSource.deletePersonById(id)
+        }
+    }
+
+    fun getPersonById(id: Long) {
+        viewModelScope.launch {
+            personDetails = collegueDataSource.getPersonById(id)
+        }
+    }
+
+    fun onFirstNameChange(value: String) {
+        firstNameText = value
+    }
+
+    fun onLastNameChange(value: String) {
+        lastNameText = value
+    }
+
+    fun onPersonDetailsDialogDismiss() {
+        personDetails = null
+    }
 }

66019c2ffa2f4d3d93d93a185b6c2b708187af8d
Omare Faruk <ofaruk0169@gmail.com>
2024-02-17 15:03:48 +0000

    just deleting some unessesary code

--- app/src/main/java/com/example/collegueclockin/ui/screens/ItemListScreen.kt
+++ app/src/main/java/com/example/collegueclockin/ui/screens/ItemListScreen.kt
@@ -2,45 +2,19 @@
 
 package com.example.collegueclockin.ui.screens
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
+
 import androidx.compose.runtime.Composable
 import com.example.collegueclockin.viewmodels.CollegueListViewModel
-
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
-
-//import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.collegueclockin.ui.navigation.Screen
 import org.koin.androidx.compose.koinViewModel
-
-import org.koin.androidx.viewmodel.ext.android.viewModel
-
 
 @Composable
 fun ItemListScreen() {
 
     val viewModel: CollegueListViewModel = koinViewModel()
-
     // observing the list of items from the ViewModel
     val itemList by viewModel.collegues.collectAsState(initial = emptyList())
 
@@ -52,50 +26,5 @@ fun ItemListScreen() {
             Text(text = "${item.firstName} ${item.lastName}")
         }
     }
-
-
-
-    //below is screen test
-
-   /* Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(
-            text = "Colleague Clock-in Application"
-        )
-
-        Spacer(modifier = Modifier.height(46.dp))
-
-        //Button layout
-
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp),
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-
-        }
-
-
-
-        //submit button
-        Button(
-            onClick = {
-                // Handle button 1 click
-            },
-            modifier = Modifier
-                .height(66.dp)
-                .padding(10.dp)
-        ) {
-            Text(text = "Enter")
-        }
-    }*/
-
-    //end of ttest screen
 
 }

0f3ef8076990f558a4b8b49b630330cbb808893d
Omare Faruk <ofaruk0169@gmail.com>
2024-03-06 16:40:52 +0000

    initial scaffold completed

--- app/src/main/java/com/example/colleagueclockin/ui/screens/MainScreen.kt
+++ app/src/main/java/com/example/colleagueclockin/ui/screens/MainScreen.kt
@@ -11,9 +11,18 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -24,6 +33,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
@@ -35,19 +45,55 @@ import com.example.colleagueclockin.viewmodels.MainScreenViewModel
 import kotlinx.coroutines.delay
 import org.koin.androidx.compose.koinViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainAppContent(
     navController: NavController,
     viewModel: MainScreenViewModel = koinViewModel()
 ) {
-
+    var text by remember { mutableStateOf("Hello, Scaffold!") }
     var password by remember { mutableStateOf("") }
 
     val colleagues = viewModel.colleagues
         .collectAsState(initial = emptyList())
         .value
 
-    Column(
+    Scaffold(
+        topBar = {
+            TopAppBar(title = { Text(text = "Colleague Clock-in Application")} )
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = { /*TODO*/ }) {
+                Icon(imageVector = Icons.Default.Add, contentDescription = "")
+            }
+        },
+        bottomBar = {
+            BottomAppBar {
+                Text(text = "Home")
+                Text(text = "Settings")
+                Text(text = "Search")
+            }
+        }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "Simple Button")
+            }
+        }
+    }
+
+
+}
+
+
+
+/*    Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(16.dp),
@@ -111,8 +157,8 @@ fun MainAppContent(
         ) {
             Text(text = "Enter")
         }
-    }
-}
+    }*/
+
 
 @Composable
 fun UserAddSignInSuccess(viewModel: MainScreenViewModel) {
